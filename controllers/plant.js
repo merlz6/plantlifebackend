5ee5c67d1d12912cb4c3d7b46feac4ef23576d72
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2021-03-06 15:06:32 -0500

    heroku backend initial

---
+++ controllers/plant.js
@@ -0,0 +1,36 @@
+const express = require('express');
+//Creating a Router (Collection of Routes)
+const router = express.Router();
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+router.get('/', (req,res)=> {
+  (async () => {
+    const response = await fetch(`https://trefle.io/api/v1/plants?token=${process.env.API_KEY}`);
+    const json = await response.json();
+      res.send(json);
+  })()
+
+
+})
+
+//search for one plant by
+router.get('/species/:search', (req,res) => {
+  (async () => {
+    const response = await fetch(`https://trefle.io/api/v1/species/search?q=${req.params.search}&&token=${process.env.API_KEY}`);
+    const json = await response.json();
+      res.send(json);
+  })()
+})
+
+//search by common name or scientific name
+router.get('/plant/:search', (req,res) => {
+  (async () => {
+    const response = await fetch(`https://trefle.io/api/v1/species/search?q=${req.params.search}&&token=${process.env.API_KEY}`);
+    const json = await response.json();
+      res.send(json);
+  })()
+})
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,50 @@
+
+const express = require('express'); //Express
+const app = express(); //Create app object
+// const mongoose = require('mongoose'); //bring in mongoose
+// const Plant = require('./models/plant.js'); //GM Model
+// const methodOverride = require('method-override'); //Method Override
+const plantController = require('./controllers/plant.js'); //GM Controller
+require('dotenv').config(); //Process.env holds all your env variables
+const port = process.env.PORT || 3000; // Port Number
+const fetch = require('node-fetch');
+// const DBURI = process.env.MONGOURI; //Mongo DB URI
+// const userController = require('./controllers/users.js'); //Users Controller - Login for this?
+// const session = require('express-session'); //Session Middlewhere
+// const User = require('./models/users.js'); // User Model
+// const bcrypt = require('bcryptjs'); //bcrypt to encrypt passwords
+
+
+// app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+// app.use(methodOverride('_method'));
+// app.use(
+//     session({
+//         secret: process.env.SECRET,
+//         resave: false,
+//         saveUninitialized: false,
+//     })
+// );
+
+app.use('/plant', plantController);
+
+
+// mongoose connection - Am I going to use DB?
+// mongoose.connect(DBURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+// mongoose.connection.once('open', () => {
+//     console.log('connected to mongo');
+// });
+
+app.get('/', (req,res)=> {
+
+res.send("howdy")
+
+
+})
+
+app.listen(port, () => {
+    console.log('listening on: ' + port);
+});
